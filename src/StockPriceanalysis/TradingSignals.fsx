3f914380192ba79835e8193ca3c2de6cec1f2f3a
Karl O'Brien <karlobrien@gmail.com>
2017-08-14 20:34:55 +0100

    Cross over strategy and trading signals

---
+++ src/StockPriceanalysis/TradingSignals.fsx
@@ -0,0 +1,48 @@
+#r "..//..//packages//FSharp.Data//lib//net40//FSharp.Data.dll"
+#I "../../packages/XPlot.GoogleCharts/lib/net45"
+#I "../../packages/Deedle/lib/net40"
+
+#r "Deedle.dll"
+#r "XPlot.GoogleCharts.dll"
+
+#load "../../packages/FsLab/FsLab.fsx"
+
+open FSharp.Data
+open System
+open System.Xml
+open XPlot.GoogleCharts
+open Deedle
+open FsLab
+
+let appleCsv = Frame.ReadCsv(__SOURCE_DIRECTORY__ + "/../data//apple_raw.csv", hasHeaders=true,inferTypes=true);
+
+let apple =
+  appleCsv
+  |> Frame.indexRowsDate "Date"
+  |> Frame.sortRowsByKey
+  |> Frame.sliceCols ["Close"]
+
+apple?twentyDay <- apple?Close |> Stats.movingMean 20
+apple?fiftyDay <- apple?Close |> Stats.movingMean 50
+apple?twoHundred <- apple?Close |> Stats.movingMean 200
+apple?difference <- apple?twentyDay - apple?fiftyDay
+
+apple?Regime <- apple |> Frame.mapRowValues (fun row ->
+  if row?difference > 0.0
+    then 1
+  else if row?difference = 0.0
+    then 0
+  else
+    -1)
+
+let janToAug = apple.Rows.[DateTime(2016, 1, 4) .. DateTime(2016, 8, 7)]
+
+apple?Signals <- Series.Sign(apple?Regime - (apple?Regime).Shift(-1))
+
+apple?Signals
+|> Chart.Line
+
+
+apple?Signals |> Series.groupInto
+  (fun k v -> v)
+  (fun len signal -> Series.countValues signal)
