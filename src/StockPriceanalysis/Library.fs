28bd27224153dec667cc0d89b4b2ad8c0e4c271e
Karl O'Brien <karlobrien@gmail.com>
2017-08-19 12:06:29 +0100

    new price loader library

--- src/StockPriceanalysis/Library.fs
+++ src/StockPriceanalysis/Library.fs
@@ -1,14 +1,41 @@
 namespace StockPriceanalysis
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
-module Library = 
-  
+open System
+open FSharp.Data
+open System.Xml
+open XPlot.GoogleCharts
+open Deedle
+open System.IO
+
+module PriceLoader =
+
+  type Price = { Open : float; Close: float; High: float; Low: float; AdjClose: float }
+
+  /// Loads Frame from csv file
+  let frameReader (name: string) =
+    let path = sprintf "/../data//%s_raw.csv" name
+    Frame.ReadCsv(__SOURCE_DIRECTORY__ + path, hasHeaders=true,inferTypes=true)
+
+
+  /// Returns a Series of <Date, Prices>
+  ///
+  /// ## Parameters
+  /// - `symbol` - apple, msft, google
+  let prices symbol =
+    frameReader symbol
+    |> Frame.indexRowsDate "Date"
+    |> Frame.sortRowsByKey
+    |> Frame.mapRowValues (fun c ->
+      {
+        Open = c?Open
+        Close = c?Close
+        High = c?High
+        Low = c?Low
+        AdjClose = c?``Adj Close``
+      })
+
+
+
   /// Returns 42
   ///
   /// ## Parameters
---
+++ src/StockPriceanalysis/SimpleCrossOverStrategy.fsx
@@ -0,0 +1,4 @@
+#load "Library.fs"
+
+open StockPriceanalysis.PriceLoader
+prices "apple"
